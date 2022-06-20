c3f0151eb77bd27d989ede5586f3dd8152b9d0cc
navigaid <navigaid@gmail.com>
2022-06-20 21:35:10 +0800

    inject-api.ts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # subshell
 
 ```
-$ deno repl --import-map=https://subqns.github.io/subshell/import_map.json
+$ deno repl --import-map=https://subqns.github.io/subshell/import_map.json --eval-file=https://subqns.github.io/subshell/inject-api.ts
 ```
---
+++ inject-api.ts
@@ -0,0 +1,7 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+
+const wsProvider = new WsProvider(
+  Deno.env.get("PROVIDER") ?? "wss://rpc.polkadot.io",
+);
+const types = JSON.parse(Deno.env.get("TYPES") ?? "{}");
+const api = await ApiPromise.create({ provider: wsProvider, types });
